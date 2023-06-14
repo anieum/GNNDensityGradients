3408f2b6ca17c086876c5ae3e5dd0d252ae59189
Jakob Semmler <j.semmler@tum.de>
2023-06-14 10:31:43 +0200

    Implement basic vtk dataset

--- datasets/vtk_dataset.py
+++ datasets/vtk_dataset.py
@@ -6,18 +6,28 @@ class VtkDataset(torch.utils.data.Dataset):
     # Todo, shuffle, batching, etc.
     # Todo, add velocity as data
 
-    def __init__(self, path, jitter=0.0):
+    def __init__(self, path, jitter=0.0, normalize=False):
+        """
+        Initialize the dataset from a list of vtk files.
+        We store densities and velocities as features and density gradients as targets.
+        All data is normalized. (todo: this should be done as an extra step)
+
+        :param paths: list of paths to vtk files
+        :param jitter: amount of noise to add to the data
+        :param normalize: whether to normalize the data
+        """
+
         if jitter != 0.0:
             self.add_noise(jitter)
 
         # todo: compose dataset from multiple files
         if not isinstance(paths, list):
             paths = [paths]
-        # How do I handle multiple files? The neighbor search is done on a per-file basis, so I can't just concatenate the files.
-        # I can calculate from particle counts how many particles are in each file, and then use that to index into the correct file.
 
         points_per_file = []
         self.n_points = 0
+        self.data = []
+        self.target = []
 
         for path in paths:
             mesh = pv.UnstructuredGrid(path)
@@ -25,37 +35,43 @@ class VtkDataset(torch.utils.data.Dataset):
             file_idx = len(points_per_file) - 1
 
             # concatenate features
-            densities = torch.tensor(mesh.point_data["density"])
-            velocities = torch.tensor(mesh.point_data["velocity"])
+            if normalize:
+                densities = self.normalize(torch.tensor(mesh.point_data["density"]))
+                velocities = self.normalize(torch.tensor(mesh.point_data["velocity"]))
+            else:
+                densities = torch.tensor(mesh.point_data["density"])
+                velocities = torch.tensor(mesh.point_data["velocity"])
+
 
             for density, velocity in zip(densities, velocities):
                 sample = (density, velocity, file_idx)
                 self.data.append(sample)
-
-            # todo, continue here
+            
+            # ok, mal überlegen, jetzt speichert jedes sample gleichzeitig einen file_idx. d.h. ich muss
 
             density_gradients = torch.tensor(mesh.point_data["density_grad"])
-            
-            self.data = data
-            self.target = density_gradients
+            self.target.append(density_gradients)
 
             self.n_points += mesh.n_points
 
-        self.data.contiguous()
+        self.target = torch.stack(self.target)
         self.target.contiguous()
+
+        # todo: check if this works
+        # self.data = torch.stack(self.data)
+        # self.data.contiguous()
 
     def __len__(self):
         return self.n_points
 
     def __getitem__(self, idx):
         # Care: Does this return dangling references? Probably!
-        return torch.tensor(self.data[idx]).view(-1), torch.tensor(self.target[idx]).view(-1)
+        data, density, file_idx = self.data[idx]
+
+        return (data, density, self.points_per_file[file_idx]), self.target[idx]
     
     def add_noise(self, noise):
         raise Exception("TODO: Implement noise")
-    
-    def get_points(self):
-        return torch.tensor(self.mesh.points)
     
     def normalize(self, data):
         mean = torch.mean(data)
