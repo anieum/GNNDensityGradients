df26792a6b6dc9af8a8a06fb54d768fad75c28bf
Jakob Semmler <j.semmler@tum.de>
2023-06-23 15:31:02 +0200

    Update density calculation so it respects boundary particles

--- utils/sph.py
+++ utils/sph.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from torch_geometric.nn import radius_graph
+from torch_geometric.nn import radius as radius_search
 from torch_scatter import scatter
 
 # Todo: use pysph or torchSPHv2
@@ -39,7 +40,7 @@ def apply_cubic_kernel_derivative_torch(r, h):
     :note: You must ensure that r is positive
     """
     # todo: enforce r is positive via nan return value or similar
-    
+
     # 3/2pi*h^3; the 3/2 is already included in the return values
     dimensional_factor = 1 / (np.pi * h * h * h)
 
@@ -77,9 +78,47 @@ def set_smoothing_length(h):
 
 set_kernel(apply_cubic_kernel_torch)
 set_kernel_grad(apply_cubic_kernel_derivative_torch)
-set_smoothing_length(0.9)
+set_smoothing_length(0.3)
 
-def get_density(point_cloud, masses, kernel=kernel, smoothing_length=smoothing_length):
+def get_neighbors(point_cloud, point_cloud_with_boundary, radius, loop=True):
+    """
+    Calculates the neighbors for each point in the point cloud.
+    :param point_cloud: point cloud of sph particles
+    :param radius: radius for the kernel function
+    :return: neighbors for each point in the point cloud
+    """
+    # TODO ENSURE THIS OUTPUTS EXACTLY THE SAME AS
+    # j, i = radius_graph(point_cloud, r=smoothing_length * 3.3, batch=None, loop=True, max_num_neighbors=2048)
+    # (with j and i swapped as output here)
+    # given the correct parameters
+
+    pairs = radius_search(x=point_cloud_with_boundary, y=point_cloud, r=radius, max_num_neighbors=2048)
+
+    if not loop:
+        pairs = pairs[:,pairs[0] != pairs[1]]
+        pairs.contiguous()  # check if this improves or worsens performance
+
+    return pairs
+
+
+def const_len_append(const, tensor_first, tensor_second):
+    """
+    Concats tensor_first with constant length of tensor_seconds values.
+    :param const: constant value
+    :param tensor_first: tensor to prepand
+    :param tensor_second: tensor to get the shape from
+    :return: tensor of same shape as tensor, but with all values set to const
+    """
+
+    new_shape = list(tensor_first.shape)
+    new_shape[0] = tensor_second.shape[0]
+
+    tensor_second_new = const * torch.ones(new_shape, dtype=tensor_second.dtype, device=tensor_second.device)
+
+    return torch.concat([tensor_first, tensor_second_new])
+
+
+def get_density(point_cloud, masses, kernel=kernel, smoothing_length=smoothing_length, boundary_box = None):
     """
     Calculates the density for each point in the point cloud.
     formula: rho_i = sum_j m_j W(|r_i - r_j|, h)
@@ -90,26 +129,35 @@ def get_density(point_cloud, masses, kernel=kernel, smoothing_length=smoothing_l
     :param smoothing_length: smoothing length for the kernel function
     :return: density rho for each point in the point cloud
     """
+    with torch.no_grad():
+        total_particles = point_cloud if boundary_box is None else torch.concat([point_cloud, boundary_box])
+        masses = masses if boundary_box is None else const_len_append(masses.view(-1)[0], masses, boundary_box)
 
-    j, i = radius_graph(point_cloud, r=smoothing_length * 3.5, batch=None, loop=True, max_num_neighbors=1024)
+        i, j = get_neighbors(
+            point_cloud = point_cloud,
+            point_cloud_with_boundary = total_particles,
+            radius = smoothing_length * 3.3,
+            loop = True,
+        )
 
-    # Calculate pairwise distances, by looking up the points in the point cloud
-    distances = torch.norm(point_cloud[i] - point_cloud[j], dim=1).view(-1, 1)
+        # Calculate pairwise distances, by looking up the points in the point cloud
+        distances = torch.norm(total_particles[i] - total_particles[j], dim=1).view(-1, 1)
 
-    # Calculate pairwise kernel values
-    kernel_values = kernel(distances / smoothing_length, smoothing_length)
-    kernel_values = masses[j].view(-1, 1) * kernel_values
+        # Calculate pairwise kernel values
+        kernel_values = kernel(distances / smoothing_length, smoothing_length)
+        kernel_values = masses[j].view(-1, 1) * kernel_values
 
-    return scatter(kernel_values, i, dim=0, dim_size = point_cloud.shape[0], reduce='add')
+        return scatter(kernel_values, i, dim=0, dim_size = point_cloud.shape[0], reduce='add')
 
 
 
-def get_spatial_density_gradient(point_cloud, masses, densities, kernel_grad=kernel_grad, smoothing_length=smoothing_length):
+
+def get_spatial_density_gradient(point_cloud, masses, densities, kernel_grad=kernel_grad, smoothing_length=smoothing_length, boundary_box = None):
     """
     Calculates the spatial density gradient for each point in the point cloud.
     formula: Grad_i rho_i = 1/rho_i sum_j m_j (rho_i + rho_j) Grad_i W_i_j
 
-    :param point_cloud: point cloud of sph particles	
+    :param point_cloud: point cloud of sph particles
     :param masses: masses of each point in the point cloud
     :param densities: densities of each point in the point cloud
     :param kernel_grad: kernel gradient function
@@ -117,9 +165,19 @@ def get_spatial_density_gradient(point_cloud, masses, densities, kernel_grad=ker
     :return: spatial density gradient Grad rho for each point in the point cloud
     """
     # TODO: check if pairs occur once or twice! (Assumption is twice.)
-    j, i = radius_graph(point_cloud, r=smoothing_length * 3.5, batch=None, loop=False, max_num_neighbors=1024)
+    total_particles = point_cloud if boundary_box is None else torch.concat([point_cloud, boundary_box])
+    masses = masses if boundary_box is None else const_len_append(masses.view(-1)[0], masses, boundary_box)
+    densities = densities if boundary_box is None else const_len_append(densities.view(-1)[0], densities, boundary_box)
+    # TODO: Which density for boundary particles?
 
-    pairwise_difference = point_cloud[j] - point_cloud[i]
+    i, j = get_neighbors(
+        point_cloud = point_cloud,
+        point_cloud_with_boundary = total_particles,
+        radius = smoothing_length * 3.3,
+        loop = False,
+    )
+
+    pairwise_difference = total_particles[j] - total_particles[i]
     pairwise_distances = torch.norm(pairwise_difference, dim=1).view(-1, 1)
     pairwise_directions = pairwise_difference / pairwise_distances
 
@@ -128,11 +186,11 @@ def get_spatial_density_gradient(point_cloud, masses, densities, kernel_grad=ker
     weight = masses[j].view(-1, 1) * pairwise_density_diffs * kernel_grad
     summed = scatter(weight, i, dim=0, dim_size = point_cloud.shape[0], reduce='add')
 
-    return summed / densities.view(-1, 1)
+    return summed / densities[:len(point_cloud)].view(-1, 1)
 
 
 
-def get_temporal_density_gradient(point_cloud, masses, velocities, kernel_grad=kernel_grad, smoothing_length=smoothing_length):
+def get_temporal_density_gradient(point_cloud, masses, velocities, kernel_grad=kernel_grad, smoothing_length=smoothing_length, boundary_box = None):
     """
     Calculates the temporal density gradient for each point in the point cloud.
     formula: Drho_i / Dt = sum_j m_j (v_i - v_j) W_Grad_i_j
@@ -145,12 +203,21 @@ def get_temporal_density_gradient(point_cloud, masses, velocities, kernel_grad=k
     :return: temporal density gradient Drho/Dt for each point in the point cloud
     """
     # ignore loops, because if i == j, then v_i - v_j = 0 => Drho_i / Dt = 0
-    j, i = radius_graph(point_cloud, r=smoothing_length * 3.5, batch=None, loop=False, max_num_neighbors=1024)
-    
-    pairwise_difference = point_cloud[j] - point_cloud[i]
+    total_particles = point_cloud if boundary_box is None else torch.concat([point_cloud, boundary_box])
+    masses = masses if boundary_box is None else const_len_append(masses.view(-1)[0], masses, boundary_box)
+    velocities = velocities if boundary_box is None else const_len_append(0., velocities, boundary_box)
+
+    i, j = get_neighbors(
+        point_cloud = point_cloud,
+        point_cloud_with_boundary = total_particles,
+        radius = smoothing_length * 3.3,
+        loop = False,
+    )
+
+    pairwise_difference = total_particles[j] - total_particles[i]
     pairwise_distances = torch.norm(pairwise_difference, dim=1)
     direction = pairwise_difference / pairwise_distances.view(-1, 1)
-    
+
     kernel_grad = kernel_grad(pairwise_distances / smoothing_length, smoothing_length).view(-1,1)
     kernel_grad = kernel_grad * direction
     velocity_diffs = velocities[i] - velocities[j]
@@ -160,7 +227,7 @@ def get_temporal_density_gradient(point_cloud, masses, velocities, kernel_grad=k
     density_diffs = masses[j].view(-1, 1) * density_diffs
 
     temporal_gradient = scatter(density_diffs, i, dim=0, dim_size = point_cloud.shape[0], reduce='add')
-    
+
     return temporal_gradient
 
 
@@ -175,7 +242,7 @@ def approximate_temporal_gradient(point_cloud_1, point_cloud_2, masses, delta_t)
     :param delta_t: time difference between the two point clouds
     :return: temporal density gradient Drho/Dt for each point in the point cloud
     """
-    
+
     # todo: do this by letting the particles move and then calculating the density gradient
 
     density_2 = get_density(point_cloud_2, masses, w)
@@ -196,13 +263,13 @@ def approximate_spatial_gradient(point_cloud, masses, w, iterations, delta_x = 0
     :param delta_x: amount of nudging
     :return: spatial density gradient Drho/Dx for each point in the point cloud
     """
-    
+
     # monte carlo inspired
     density_gradient = torch.zeros_like(point_cloud)
 
     for _ in range(iterations):
         diff = torch.random_like(point_cloud) * delta_x
-        
+
         density_nudged = get_density(point_cloud + diff, masses, w)
         density = get_density(point_cloud, masses, w)
 
