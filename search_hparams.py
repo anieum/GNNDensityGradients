1c664d6db5fc72d3d74fb6cd2800ccda8b9c678e
Jakob Semmler <j.semmler@tum.de>
2023-07-04 17:02:22 +0200

    Fix hparam search disk blow up

--- search_hparams.py
+++ search_hparams.py
@@ -3,8 +3,9 @@ from utils.train_helper import *
 from models.cconv import CConvModel
 from datasets.density_data_module import DensityDataModule
 from pytorch_lightning.loggers import TensorBoardLogger
+import ray
 from ray.train.lightning import LightningTrainer, LightningConfigBuilder
-from ray import air, tune, init
+from ray import air, tune
 from ray.air.config import RunConfig, ScalingConfig, CheckpointConfig
 from ray.tune.schedulers import ASHAScheduler
 import torch.cuda
@@ -18,7 +19,6 @@ num_epochs = 5
 # Number of sampls from parameter space
 num_samples = 2
 
-
 hparams = {
     # "layer_1_size": tune.choice([32, 64, 128]),
     # "layer_2_size": tune.choice([64, 128, 256]),
@@ -29,52 +29,69 @@ hparams = {
     'data_split': (0.7, 0.15, 0.15),
     'batch_size': 10,        # care, this is used in the model and datamodule
     'shuffle': True,
-    'cache': True,            # Preprocess and preload dataset into memory
+    'cache': False,            # Preprocess and preload dataset into memory
     'device': 'cuda'
 }
 
 datamodule = DensityDataModule(data_dir=hparams['dataset_dir'], batch_size=hparams['batch_size'], data_split=hparams['data_split'], shuffle=hparams['shuffle'], cache=hparams['cache'], device=hparams['device'])
 logger = TensorBoardLogger("lightning_logs", name="cconv-hparam-search", version=".")
+
+# DO NOT CACHE THE DATAMODULE AND PASS IT DIRECTLY WITHOUT LOADERS.
+# OTHERWISE RAY TUNE WILL SERIALIZE THE ENTIRE DATASET AND BLOW UP MEMORY AND DISK SPACE
+datamodule.setup("fit")
+torch.cuda.empty_cache()
+
+train_loader = datamodule.train_dataloader()
+val_loader = datamodule.val_dataloader()
+
+
 lightning_config = (
     LightningConfigBuilder()
     .module(cls=CConvModel, hparams=hparams)
     .trainer(max_epochs=num_epochs, logger=logger, accelerator="gpu", enable_progress_bar=False)
-    .fit_params(datamodule=datamodule)
-    .checkpointing(monitor="val_loss", mode="min", save_top_k=3)
+    .fit_params(train_dataloaders=train_loader, val_dataloaders=val_loader)
+    .checkpointing(monitor="val_loss", mode="min", save_top_k=2)
     .build()
 )
-run_config = RunConfig(
-    checkpoint_config = CheckpointConfig(
-        num_to_keep=3,
-        checkpoint_score_attribute="val_loss",
-        checkpoint_score_order="min",
+
+ray.shutdown()
+ray.init(num_cpus=6, num_gpus=1)
+
+lightning_trainer = LightningTrainer(
+    scaling_config = ScalingConfig(
+        num_workers=1,
+        use_gpu=True,
+        resources_per_worker={"CPU": 2, "GPU": 0.5}
     ),
+    run_config = RunConfig(
+        checkpoint_config = CheckpointConfig(
+            num_to_keep=2,
+            checkpoint_score_attribute="val_loss",
+            checkpoint_score_order="min",
+        ),
+    )
 )
 
-
-init(num_cpus=2, num_gpus=1)
-scaling_config = ScalingConfig(num_workers=1, use_gpu=True, resources_per_worker={"CPU": 1, "GPU": 0.5})
-
-# Define a base LightningTrainer without hyper-parameters for Tuner
-lightning_trainer = LightningTrainer(scaling_config=scaling_config, run_config=run_config)
-datamodule.setup("Initialize")
-datamodule.to('cpu')
-def tune_mnist_asha(num_samples=10):
+def tune_models(num_samples=10):
     scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
+
+    # Todo: search algorithm (the standard is either hyperopt, grid or random search)
+    # https://docs.ray.io/en/latest/tune/api/suggestion.html
 
     tuner = tune.Tuner(
         lightning_trainer,
-        param_space={"lightning_config": lightning_config},
-        tune_config=tune.TuneConfig(
-            metric="val_loss",
-            mode="min",
-            num_samples=num_samples,
-            scheduler=scheduler,
+        param_space = {"lightning_config": lightning_config},
+        tune_config = tune.TuneConfig(
+            metric      = "val_loss",
+            mode        = "min",
+            num_samples = num_samples,
+            scheduler   = scheduler,
         )
     )
+
     results = tuner.fit()
     best_result = results.get_best_result(metric="val_loss", mode="max")
     best_result
 
 print("Cuda is available: ", torch.cuda.is_available())
-tune_mnist_asha(num_samples=num_samples)
+tune_models(num_samples=num_samples)
