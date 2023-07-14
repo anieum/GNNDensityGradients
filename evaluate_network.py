63cf5ad9b549a30b49ee939637f152eda32f0bc3
Jakob Semmler <j.semmler@tum.de>
2023-07-14 16:30:18 +0200

    temp

---
+++ evaluate_network.py
@@ -0,0 +1,37 @@
+
+
+# This script generations the evaluation metrics for the network
+# The metrics are:
+# 1. Accuracy
+# 2. Precision
+# 3. Recall
+# 4. F1 Score
+# 5. Confusion Matrix
+# 6. ROC Curve
+# 7. AUC Score
+# 8. PR Curve
+# 9. Average Precision Score
+# 10. Loss
+# 11. Mean Absolute Error
+# 12. Mean Squared Error
+# 13. Root Mean Squared Error
+# 14. R2 Score
+# 15. Explained Variance Score
+# 16. Mean Absolute Percentage Error
+# 17. Mean Percentage Error
+# 18. Mean Absolute Scaled Error
+# 19. Median Absolute Error
+# 20. Mean Squared Log Error
+# 21. Mean Absolute Log Error
+# 22. Symmetric Mean Absolute Percentage Error
+# 23. Mean Absolute Deviation
+# 24. Mean Absolute Percentage Deviation
+# 25. Median Absolute Percentage Error
+# 26. Median Absolute Percentage Deviation
+# 27. Mean Directional Accuracy
+# 28. Mean Absolute Scaled Error
+# 29. Median Absolute Scaled Error
+# 30. Mean Absolute Error Percentage
+# 31. Mean Percentage Error
+# 32. Mean Absolute Percentage Error
+
