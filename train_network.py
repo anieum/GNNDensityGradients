d368bb2a950ebcf323508d7782f806cbab754d52
Jakob Semmler <j.semmler@tum.de>
2023-08-02 15:00:06 +0200

    Update train_network for ray tune checkpoints

--- train_network.py
+++ train_network.py
@@ -7,6 +7,9 @@ from models.cconv import CConvModel
 from datasets.vtk_dataset import VtkDataset
 from datasets.density_data_module import DensityDataModule
 
+
+
+
 hparams = {
     # General ---------------------------------------------------
 
@@ -17,14 +20,14 @@ hparams = {
     'cache'       : True,                                         # Preprocess and preload dataset into memory
 
     # Training
-    'batch_size' : 4,
-    'lr'         : 2e-3,
-    'device'     : 'cuda' if torch.cuda.is_available() else 'cpu',
+    'batch_size'    : 2,
+    'learning_rate' : 2e-3,
+    'device'        : 'cuda' if torch.cuda.is_available() else 'cpu',
 
     'num_training_nodes'  : 1,    # number of nodes to train on (e.g. 1 GPU)
     'num_workers'         : 0,    # number of workers for dataloader (e.g. 2 worker threads)
-    'num_epochs'          : 15,   # Per epoch with 1000 files containing 6600 samples expect 6 minutes on a 1080 Ti
-    'limit_train_batches' : 0.1,  # Use only 10% of the training data per epoch; default is 1.0
+    'num_epochs'          : 25,   # Per epoch with 1000 files containing 6600 samples expect 6 minutes on a 1080 Ti
+    'limit_train_batches' : 0.5,  # Use only 10% of the training data per epoch; default is 1.0
     'limit_val_batches'   : 0.1,  # Use only 10% of the validation data per epoch; default is 1.0
 
     # Logging
@@ -32,9 +35,10 @@ hparams = {
     'val_every_n_epoch' : 3,
 
     # Checkpoints
-    'load_checkpoint' : False,
-    'save_path'       : 'lightning_logs',
-    'load_path'       : 'lightning_logs/version_42.ckpt',
+    'load_checkpoint' : True,
+    'save_path'       : 'lightning_logs/best',
+    'load_path'       : '/home/jakob/ray_results3/LightningTrainer_2023-07-31_17-28-20/00093_93/rank_0/logs/srch/checkpoints/last.ckpt',
+    'params_path'     : '/home/jakob/ray_results3/LightningTrainer_2023-07-31_17-28-20/00093_93/params.json',
     'model'           : CConvModel,
 
     # CConv parameters -------------------------------------------
@@ -62,8 +66,7 @@ hparams = {
 validate_hparams(hparams)
 
 # Load model
-m = hparams['model']
-model = m.load_from_checkpoint(hparams['load_path']) if hparams['load_checkpoint'] else m(hparams)
+model = hparams['model'](hparams)
 
 # Datasets
 density_data = DensityDataModule(
@@ -101,7 +104,18 @@ trainer = pl.Trainer(
 # model.learning_rate = find_learning_rate(trainer, model, density_data)
 
 # Train
-trainer.fit(model, datamodule=density_data, ckpt_path=hparams['load_path'])
+if hparams['load_checkpoint']:
+    # Load hyperparameters from checkpoint; only overwrite model specific parameters
+    print("Loading hyperparameters from checkpoint...")
+    loaded_hparams = load_hparams(file_path=hparams['params_path'])
+    hparams        = update_hparams(hparams=hparams, new_hparams=loaded_hparams)
+    model          = hparams['model'].load_from_checkpoint(hparams['load_path'], hparams=hparams)
+    print("Loaded hyperparameters from checkpoint:", hparams)
+
+    print("Resuming training...")
+    trainer.fit(model, datamodule=density_data, ckpt_path=hparams['load_path'])
+else:
+    trainer.fit(model, datamodule=density_data)
 
 # Save model
 save_checkpoint(trainer, model, hparams['save_path'])
